27db249f2f8f1c3a4eacb95caddcdc3bea57ad56
JackCoote <s222795@uos.ac.uk>
2023-11-19 20:12:28 +0000

    MenuChanges&Fixes

--- Unity/Horde Survivial/Assets/Jack/Scripts/UIFadeOut.cs
+++ Unity/Horde Survivial/Assets/Jack/Scripts/UIFadeOut.cs
@@ -7,41 +7,22 @@ public class UIFadeOut : MonoBehaviour
 {
     public float fadeDuration = 2.0f;
     private float currentAlpha = 1.0f;
+
     private Image image;
 
     private void Start()
     {
         image = GetComponent<Image>();
-        Time.timeScale = 0;
     }
 
     private void Update()
     {
+        currentAlpha -= Time.deltaTime / fadeDuration;
+        image.color = new Color(image.color.r, image.color.g, image.color.b, currentAlpha);
 
-    }
-
-    // Public method to start the fade-out when called.
-    public void FadeOut()
-    {
-        // Reset the alpha value.
-        currentAlpha = 1.0f;
-        // Enable the GameObject in case it was disabled previously.
-        gameObject.SetActive(true);
-        // Call the coroutine for smoother fading.
-        StartCoroutine(FadeOutCoroutine());
-    }
-
-    // Coroutine for smoother fading.
-    private IEnumerator FadeOutCoroutine()
-    {
-        while (currentAlpha > 0.0f)
+        if (currentAlpha <= 0.0f)
         {
-            currentAlpha -= Time.deltaTime / fadeDuration;
-            image.color = new Color(image.color.r, image.color.g, image.color.b, currentAlpha);
-            yield return null;
+            gameObject.SetActive(false);
         }
-
-        // After the fade-out is complete, you can disable the GameObject.
-        gameObject.SetActive(false);
     }
 }
---
+++ Unity/Horde Survivial/Assets/Jack/Scripts/UIFadeOutBut.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFadeOutBut : MonoBehaviour
+{
+    public float fadeDuration = 2.0f;
+    private float currentAlpha = 1.0f;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    // Public method to start the fade-out when called.
+    public void FadeOut()
+    {
+        // Reset the alpha value.
+        currentAlpha = 1.0f;
+        // Enable the GameObject in case it was disabled previously.
+        gameObject.SetActive(true);
+        // Call the coroutine for smoother fading.
+        StartCoroutine(FadeOutCoroutine());
+    }
+
+    // Coroutine for smoother fading.
+    private IEnumerator FadeOutCoroutine()
+    {
+        while (currentAlpha > 0.0f)
+        {
+            currentAlpha -= Time.deltaTime / fadeDuration;
+            image.color = new Color(image.color.r, image.color.g, image.color.b, currentAlpha);
+            yield return null;
+        }
+
+        // After the fade-out is complete, you can disable the GameObject.
+        gameObject.SetActive(false);
+    }
+}
