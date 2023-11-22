e3274661e5f6e067c6b83872ddb1a0a906671bb2
The-Holy <s231948@uos.ac.uk>
2023-11-22 09:53:49 +0000

    show achievements in the main menu code
    sdgfsdfg

--- Unity/Horde Survivial/Assets/Scripts/Callum Scripts/Achievements.cs
+++ Unity/Horde Survivial/Assets/Scripts/Callum Scripts/Achievements.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Achievements : MonoBehaviour
 {
+    public GameObject Head1;
+    public GameObject Head2;
+    public GameObject Head3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,28 @@ public class Achievements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (PlayerPrefs.GetInt("Total Time Survived4") >= 50)
+        {
+            Head1.SetActive(true);
+            Debug.Log("Achievement unlocked, survived for 20 seconds");
+        }
+        if (PlayerPrefs.GetInt("Total Time Survived4") < 50)
+        {
+            Head1.SetActive(false);
+            Debug.Log("Achievement unlocked, survived for 20 seconds");
+        }
 
+
+        if (PlayerPrefs.GetInt("Total Enemies Killed") >= 20)
+        {
+            Head2.SetActive(true);
+            Debug.Log("Achievement unlocked, killed a total of 20 enemies");
+        }
+
+        if (PlayerPrefs.GetInt("Total Enemies Killed") < 20)
+        {
+            Head2.SetActive(false);
+            Debug.Log("Achievement unlocked, killed a total of 20 enemies");
+        }
     }
 }
